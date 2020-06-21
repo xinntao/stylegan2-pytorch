5377e71e02916b1f8f297c9828b43feab733a191
XintaoWang <wxt1994@126.com>
2020-06-21 11:39:54 +0800

    update format of generator

--- generate.py
+++ generate.py
@@ -2,24 +2,30 @@ import argparse
 
 import torch
 from torchvision import utils
-from model import Generator
 from tqdm import tqdm
+
+from model import Generator
+
+
 def generate(args, g_ema, device, mean_latent):
 
     with torch.no_grad():
         g_ema.eval()
         for i in tqdm(range(args.pics)):
-           sample_z = torch.randn(args.sample, args.latent, device=device)
+            sample_z = torch.randn(args.sample, args.latent, device=device)
 
-           sample, _ = g_ema([sample_z], truncation=args.truncation, truncation_latent=mean_latent)
-           
-           utils.save_image(
-            sample,
-            f'sample/{str(i).zfill(6)}.png',
-            nrow=1,
-            normalize=True,
-            range=(-1, 1),
-        )
+            sample, _ = g_ema([sample_z],
+                              truncation=args.truncation,
+                              truncation_latent=mean_latent)
+
+            utils.save_image(
+                sample,
+                f'sample/{str(i).zfill(6)}.png',
+                nrow=1,
+                normalize=True,
+                range=(-1, 1),
+            )
+
 
 if __name__ == '__main__':
     device = 'cuda'
@@ -31,7 +37,8 @@ if __name__ == '__main__':
     parser.add_argument('--pics', type=int, default=20)
     parser.add_argument('--truncation', type=float, default=1)
     parser.add_argument('--truncation_mean', type=int, default=4096)
-    parser.add_argument('--ckpt', type=str, default="stylegan2-ffhq-config-f.pt")
+    parser.add_argument(
+        '--ckpt', type=str, default="stylegan2-ffhq-config-f.pt")
     parser.add_argument('--channel_multiplier', type=int, default=2)
 
     args = parser.parse_args()
@@ -40,8 +47,10 @@ if __name__ == '__main__':
     args.n_mlp = 8
 
     g_ema = Generator(
-        args.size, args.latent, args.n_mlp, channel_multiplier=args.channel_multiplier
-    ).to(device)
+        args.size,
+        args.latent,
+        args.n_mlp,
+        channel_multiplier=args.channel_multiplier).to(device)
     checkpoint = torch.load(args.ckpt)
 
     g_ema.load_state_dict(checkpoint['g_ema'])
